d73c5d6237399cc616a565a5724757376480db5c
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-17 15:40:12 +0200

    Added categories for mode of transportation

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,6 +3,7 @@ class Journey < ApplicationRecord
   belongs_to :buddy
   enum journey_status: %i[inactive started completed], _default: "inactive"
   enum buddy_status: %i[unknown accepted declined], _default: "unknown"
-  validates :starting_point, :destination, :mode_of_transportation, presence: true
+  validates :starting_point, :destination, presence: true
   validates :time_estimate, presence: true, inclusion: { in: [10, 20, 30, 40, 50, 60] }
+  validates :mode_of_transportation, presence: true, inclusion: { in: ["Bus", "Train", "Taxi/Uber", "Car", "Foot"] }
 end
