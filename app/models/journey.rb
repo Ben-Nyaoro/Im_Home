a005603ab1fe392b00de6dc6ae888470d67848f6
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-17 12:59:23 +0200

    corrected error in journey model

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,6 +3,6 @@ class Journey < ApplicationRecord
   belongs_to :buddy
   enum journey_status: %i[inactive started completed], _default: "inactive"
   enum buddy_status: %i[unknown accepted declined], _default: "unknown"
-  validates :starting_point, :destination, :mode_of_transport, presence: true
+  validates :starting_point, :destination, :mode_of_transportation, presence: true
   validates :time_estimate, presence: true, inclusion: { in: %i[10 20 30 40 50 60] }
 end
