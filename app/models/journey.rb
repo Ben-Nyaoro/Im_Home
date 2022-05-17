c70814e23bc6b4774b2faf383a0636f431bb58fa
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-17 09:54:03 +0200

    Changes buddy_status default

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -2,7 +2,7 @@ class Journey < ApplicationRecord
   belongs_to :user
   belongs_to :buddy
   enum journey_status: %i[inactive started completed], _default: "inactive"
-  enum buddy_status: %i[accepted declined]
+  enum buddy_status: %i[unknown accepted declined], _default: "unknown"
   validates :starting_point, :destination, :mode_of_transport, presence: true
   validates :time_estimate, presence: true, inclusion: { in: %i[10 20 30 40 50 60] }
 end
