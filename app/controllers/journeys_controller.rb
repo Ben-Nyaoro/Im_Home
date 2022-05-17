90d2e68d8cb1660c154260a872aff0c49f3b7a0d
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-17 09:26:34 +0200

    Journeys Controller part 2

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,12 +1,12 @@
 class JourneysController < ApplicationController
-  before_action :set_journey, only: [:show, :edit, :destoy, :update]
+  before_action :set_journey, only: %i[show edit destoy update]
 
   def index
     @user = current_user
     @journeys = Journey.all
     @user_journeys = []
     @journeys.each do |journey|
-      @user_journeys << journey if journey.user == current_user
+      @user_journeys << journey if journey.user == current_user && journey.journey_status == "completed"
     end
     return @user_journeys
   end
@@ -20,6 +20,29 @@ class JourneysController < ApplicationController
 
   def create
     @journey = Journey.new(journey_params)
+    @journey.user = current_user
+    journey.save!
+    if @journey.save!
+      @journey.journey_status = "started"
+      redirect_to journey_path(@journey)
+    else
+      render :new, notice: "Your journey could not be started."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @journey.update(journey_params)
+      redirect to @journey, notice: "Your journey was successfully updated. Take care!"
+    else
+      render :edit, notice: "Please check your details"
+    end
+  end
+
+  def destroy
+    @journey.journey_status = "completed"
   end
 
   private
@@ -29,6 +52,6 @@ class JourneysController < ApplicationController
   end
 
   def journey_params
-    params.require(:journey).permit(:starting_point, :destination, :mode_of_transportation, :time_estimate)
+    params.require(:journey).permit(:starting_point, :destination, :mode_of_transportation, :time_estimate, :buddy)
   end
 end
--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,8 +1,8 @@
 class Journey < ApplicationRecord
   belongs_to :user
   belongs_to :buddy
-  enum journey_status: %i[started ongoing completed]
+  enum journey_status: %i[inactive started completed], _default: "inactive"
   enum buddy_status: %i[accepted declined]
   validates :starting_point, :destination, :mode_of_transport, presence: true
-  validates :time_estimate, presence: true, inclution: { in: %i[10 20 30 40 50 60] }
+  validates :time_estimate, presence: true, inclusion: { in: %i[10 20 30 40 50 60] }
 end
