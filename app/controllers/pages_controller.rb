ec078e5754b68154b95718a76fba38ec4de622c0
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-05-19 20:57:56 +0200

    Bug fix for buddy iteration on the home page for signed-in and logged out users in the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-		@buddies = Buddy.all
+    if signed_in?
+			@buddies = current_user.buddies
+		end
   end
 end
