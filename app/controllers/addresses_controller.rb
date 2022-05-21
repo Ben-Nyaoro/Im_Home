00e91377e6f9dee2a99ce6eb796a6878b55fb7c0
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-21 15:44:13 +0200

    added the update safe place

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,19 +1,37 @@
 class AddressesController < ApplicationController
+  before_action :set_address, only: %i[edit update]
 
   def new
     @address = Address.new
   end
 
   def create
+    @user = current_user
     @address = Address.create!(address_params)
+
     if @address.save
       redirect_to new_safe_place_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @address.update(address_params)
+      redirect_to safe_places_path, notice: "Your safe place was successfully updated."
+    else
+      render :edit, notice: "Please check your safe place's details"
     end
   end
 
   private
 
   def address_params
-    params.require(:address).permit(:address_line1, :address_line1, :postcode, :city)
+    params.require(:address).permit(:address_line1, :address_line2, :postcode, :city)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
   end
 end
