51ce249fcf5c7f41c249d2e83e0302e446e8c2a4
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-24 15:10:10 +0200

    Toggle button for address / safe place

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -3,46 +3,62 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["starting", "destination", "startingsafe", "destisafe"]
 
-  connect() {
-    console.log("Hello")
-    console.log(this.startingsafeTarget.children[0].lastChild)
-
-  }
-
-  showTargetsStart() {
-    this.startingsafeTarget.children[0].lastChild.disabled = true
-    this.startingsafeTarget.hidden = true
-    if(this.startingTarget.children[0].lastChild != null) {
-      this.startingTarget.children[0].lastChild.disabled = false
-    }
-    this.startingTarget.hidden = false
-
+  toggleTargetsStart() {
+    let startingSafe = this.startingsafeTarget.children[0].lastChild
+    if(startingSafe != null) {
+      startingSafe.disabled = !startingSafe.disabled
+    };
+    this.startingsafeTarget.hidden = !(this.startingsafeTarget.hidden)
+    const startingForm = this.startingTarget.children
+    this.startingTarget.hidden = !(this.startingTarget.hidden)
+    if(startingForm != null) {
+      startingForm[0].disabled = !startingForm[0].disabled
+      startingForm[1].disabled = !startingForm[1].disabled
+      startingForm[2].disabled = !startingForm[2].disabled
+      startingForm[3].disabled = !startingForm[3].disabled
+      }
     };
 
-    showTargetsDesti() {
-      this.destisafeTarget.children[0].lastChild.disabled = true
-      this.destisafeTarget.hidden = true
-      if (this.destinationTarget.children[0].lastChild != null) {
-        this.destinationTarget.children[0].lastChild.disabled = false
-      }
-      this.destinationTarget.hidden = false
 
-      };
+  toggleTargetsDesti() {
+    let destiSafe = this.destisafeTarget.children[0].lastChild
+    if (destiSafe != null) {
+    destiSafe.disabled = !destiSafe.disabled
+    };
+    this.destisafeTarget.hidden = !(this.destisafeTarget.hidden)
+    const destiForm = this.destinationTarget.children
+    this.destinationTarget.hidden = !(this.destinationTarget.hidden)
+    if(destiForm != null) {
+      destiForm[0].disabled = !destiForm[0].disabled
+      destiForm[1].disabled = !destiForm[1].disabled
+      destiForm[2].disabled = !destiForm[2].disabled
+      destiForm[3].disabled = !destiForm[3].disabled
+    }
+    };
 
+      // connect() {
+  //   console.log(this.startingTarget.children)
+  // };
 
-  hideTargets() {
-    this.hideableTargets.forEach(el => {
-      el.hidden = true
-      el.disabled = true
-    });
-  }
+  // showTargetsStart() {
+  //   this.startingsafeTarget.children[0].lastChild.disabled = true
+  //   this.startingsafeTarget.hidden = true
+  //   if(this.startingTarget.children[0].lastChild != null) {
+  //     (this.startingTarget.children).forEach((element) => {
+  //       element.disabled = false
+  //     });
+  //     };
+  //   this.startingTarget.hidden = false
+  //   };
 
-  toggleTargets() {
-    this.hideableTargets.forEach((el) => {
-      el.disabled = !el.disabled
-    });
-  }
+  // showTargetsDesti() {
+  //   this.destisafeTarget.children[0].lastChild.disabled = true
+  //   this.destisafeTarget.hidden = true
+  //   if (this.destinationTarget.children[0].lastChild != null) {
+  //     this.destinationTarget.children[0].lastChild.disabled = false
+  //   };
+  //   this.destinationTarget.hidden = false
 
-
+  //   };
 
 }
