2267a3b02d00f8a78d7526c5dec6eaca10039a59
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-06-03 14:40:33 +0200

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home welcome safe_places_intro buddy_intro]
 
   def home
-    # if signed_in?
-    #   redirect_to landing_path
-    # end
+    if signed_in?
+      redirect_to landing_path
+    end
   end
 
   def profile
