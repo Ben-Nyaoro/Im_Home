3d12db89d45e149c919b0b54f4e8b96feca7b7c6
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-06-03 15:17:47 +0200

    To my surprise I managed to override the default devise  route for user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :profile_picture])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :profile_picture, :email, :password, :password_confirmation, :current_password])
 	end
+
 end
