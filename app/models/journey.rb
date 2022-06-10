569fb45c1994a660b16c289e2968b3c3104dedd7
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-06-10 12:23:01 +0200

    removed one minute from journey

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -6,7 +6,7 @@ class Journey < ApplicationRecord
   belongs_to :destination, class_name: "Address"
   enum journey_status: %i[inactive started completed], _default: "inactive"
   enum buddy_status: %i[unknown accepted declined], _default: "unknown"
-  validates :time_estimate, presence: true, inclusion: { in: [1, 10, 20, 30, 40, 50, 60] }
+  validates :time_estimate, presence: true, inclusion: { in: [10, 20, 30, 40, 50, 60] }
   validates :mode_of_transportation, presence: true, inclusion: { in: ["Bus", "Train", "Taxi/Uber", "Car", "Foot"] }
 
   accepts_nested_attributes_for :starting_point
